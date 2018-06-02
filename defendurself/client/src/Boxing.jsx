22cbb4d7f82e7fdbe32b42f1b0987761d5d1913e
Jason Green <jasongreen@ool-ad02008f.dyn.optonline.net>
2018-06-02 17:01:08 -0400

    stuff

--- defendurself/client/src/Boxing.jsx
+++ defendurself/client/src/Boxing.jsx
@@ -10,7 +10,44 @@ class Boxing extends Component {
     this.state = {
       boxing:[]
     };
+    this.addGym = this.addGym.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.editGym = this.editGym.bind(this);
+    // this.destoryGym = this.destroy.bind(this);
+    // this.refreshFeed = this.refreshFeed.bind(this);
 }
+
+  addGym(){
+
+
+  }
+
+  editGym(e){
+    e.persist();
+    e.preventDefault();
+    fetch(`http://localhost:3001/boxing/${this.state.post.id}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'image': this.state.post.image,
+        'description': e.target.value
+      })
+    })
+      .then(() => this.setState ({
+        description: e.target.value
+      }))
+      .then(() => this.boxing());
+    }
+
+  handleSubmit(e){
+      this.refs.editGym.value = '';
+    }
+  destoryGym(){
+
+  }
+
+
+
 
   componentDidMount() {
     fetch('http://localhost:3001/boxing', {
@@ -27,20 +64,35 @@ class Boxing extends Component {
   }
 
   renderItems() {
-      console.log('inside renderItems')
+      // console.log('inside renderItems')
       return this.state.boxing.map((elem, i) => (
           <div key={i} className="boxing-container">
+          <div className="boxing">
+          <header className="boxing-header">
+          </header>
+          <p className= "boxingdesc">
+            Below are boxing gyms in the New York City area. Scroll though,
+            find your style and see if they're near by? Is this list missing
+            a gym you know? Add it!
+          </p>
+        </div>
            <img className="box-image" src={elem.image}/>
            <div className="boxing-info">{elem.name}</div>
            <div className="boxing-type">{elem.type}</div>
            <div className="boxing-location">{elem.location}</div>
-
+           <div className="edit">
+              <input name="edit gym" placeholder="Edit this gym"
+                onChange={this.editGym}
+                ref="editMe" />
+            <div onClick={this.handleSubmit}><i className="fas fa-pencil-alt"></i></div>
+          </div>
           </div>
       ))
   }
 
   render(){
-    if(true){ return  ( this.renderItems() ) }
+    if(true)
+      { return  ( this.renderItems() ) }
     return (
         <div className="boxing">
           <header className="boxing-header">
